98ed5d0403c0cdff0be29dd79de30aacb8c07c5e
Focus9752 <marcuskrogsgaard@outlook.de>
2023-09-24 20:42:01 +0200

    Update UA3.py

--- UA3/UA3.py
+++ UA3/UA3.py
@@ -4,6 +4,7 @@ from sympy import *
 
 y = Symbol("y")
 
+# Navnet "lambda" er forbudt i python da det bruges til anonyme funktioner
 lmda = 1
 sol1 = nsolve(y * tan(y) - sqrt(lmda - y**2), y, (0, sqrt(lmda)))
 
